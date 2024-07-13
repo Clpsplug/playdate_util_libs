90a14bfe9993d3b2631128f46999f53fffd7ac82
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2024-07-13 13:10:45 +0900

    Make install script

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ! which cmake; then
+  echo "You need cmake!"
+  return 1
+fi
+
+builder=""
+
+if which make; then
+   echo "Using make to build"
+   builder="make"
+elif which ninja; then
+    echo "Using ninja to build"
+    builder="ninja"
+fi
+
+if [ -z $builder ]; then
+    echo "make/ninja both missing! install either one (if You have both, make will be used)."
+    return 1
+fi 
+
+function build() {
+  case "$2" in
+    "simulator")
+      build_dir="build_sim";;
+    "device")
+      build_dir="build";;
+    *)
+      echo "Unknown build type";;
+  esac
+  
+  echo "Building $1 for $2"
+  
+  mkdir -p "$1/$build_dir"
+  pushd "$1/$build_dir" > /dev/null || exit 2
+  if [ $2 == "device" ]; then
+    cmake .. -DCMAKE_TOOLCHAIN_FILE="$PLAYDATE_SDK_PATH"/C_API/buildsupport/arm.cmake
+  else
+    cmake ..
+  fi 
+  "$builder"
+  "$builder" install
+  popd > /dev/null || exit 2
+}
+
+# exclude include and lib directory
+for dir in */; do
+  case $dir in
+    "include/"|"lib/") ;;
+    *) 
+      build "$dir" "device"
+      build "$dir" "simulator"
+      ;;
+  esac
+done
