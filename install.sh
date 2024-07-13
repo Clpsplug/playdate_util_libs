26c2af58c4dc0c46b44967be7e702ddc48fc7abd
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2024-07-13 23:25:55 +0900

    Update install script to use CMake (which is cleaner)

--- install.sh
+++ install.sh
@@ -2,52 +2,53 @@
 
 set -e
 
-if ! which cmake; then
-  echo "You need cmake!"
-  return 1
-fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-builder=""
-
-if which make; then
-   echo "Using make to build"
-   builder="make"
-elif which ninja; then
-    echo "Using ninja to build"
-    builder="ninja"
-fi
-
-if [ -z $builder ]; then
-    echo "make/ninja both missing! install either one (if You have both, make will be used)."
-    return 1
-fi 
-
-function build() {
-  case "$2" in
-    "simulator")
-      build_dir="build_sim";;
-    "device")
-      build_dir="build";;
-    *)
-      echo "Unknown build type";;
-  esac
-  
-  echo "Building $1 for $2"
-  
-  mkdir -p "$1/$build_dir"
-  pushd "$1/$build_dir" > /dev/null || exit 2
-  if [ $2 == "device" ]; then
-    cmake .. -DCMAKE_TOOLCHAIN_FILE="$PLAYDATE_SDK_PATH"/C_API/buildsupport/arm.cmake
-  else
-    cmake ..
-  fi 
-  "$builder"
-  "$builder" install
-  popd > /dev/null || exit 2
+function build_and_install() 
+{
+  echo "-- Building for $2 --"
+  pushd "${SCRIPT_DIR:?}/$1" > /dev/null || ( echo "pushd to ${SCRIPT_DIR:?}/$1 failed"; exit 1 )
+    case "$2" in
+      "device")
+      cmake .. -DCMAKE_TOOLCHAIN_FILE="$PLAYDATE_SDK_PATH/C_API/buildsupport/arm.cmake"
+      ;;
+      "sim")
+      cmake ..
+      ;;
+      *)
+        echo "Unknown build target $2";
+        exit 1;
+        ;;
+    esac
+    cmake --build . --target clean
+    cmake --build . --target build_all
+    cmake --build . --target install_all
+  popd > /dev/null || ( echo "popd failed"; exit 1 )
 }
 
-# Specify the order to get rid of dependency issue
-build "shorthand" "device"
-build "shorthand" "simulator"
-build "scene_engine" "device"
-build "scene_engine" "simulator"
+if [ -z "$SCRIPT_DIR" ]; then
+    echo "Could not get script directory... this is odd."
+    exit 1
+fi 
+
+if [ -d "$SCRIPT_DIR/include" ]; then
+    /bin/rm -r "${SCRIPT_DIR:?}/include"
+fi
+
+if [ -d "$SCRIPT_DIR/lib" ]; then
+    /bin/rm -r "${SCRIPT_DIR:?}/lib"
+fi 
+
+if [ -d "$SCRIPT_DIR/build" ]; then
+  /bin/rm -r "${SCRIPT_DIR:?}/build"
+fi
+
+if [ -d "$SCRIPT_DIR/build_sim" ]; then
+  /bin/rm -r "${SCRIPT_DIR:?}/build_sim"
+fi
+
+mkdir -p "${SCRIPT_DIR:?}/build"
+mkdir -p "${SCRIPT_DIR:?}/build_sim"
+
+build_and_install build device
+build_and_install build_sim sim
